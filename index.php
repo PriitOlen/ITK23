2a26391651d7112b34fe09ff7c33185e9f11e459
PriitOlen <101823935+PriitOlen@users.noreply.github.com>
2024-04-26 14:25:00 +0300

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,8 @@
   </head>
   <body>
     <h1>Meie projekti pealeht</h1>
-    <img style="display: block;-webkit-user-select: none;margin: auto;background-color: hsl(0, 0%, 90%);transition: background-color 300ms;" src="https://static.giga.de/wp-content/uploads/2024/04/kurwa-bober-GettyImages-92911783-rcm480x270u.jpg">
+    <img src="https://static.giga.de/wp-content/uploads/2024/04/kurwa-bober-GettyImages-92911783-rcm480x270u.jpg">
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTldVZTWdg_M59wcTwedXYZfEIE16imVGuUSUSfMrOHoA&s">
   <p>See leht arvestab ka muudatustega</p> 
   </body>
 </html>
