d52c792d5b502f15188bdc42fd0b6bcc604e01e7
PriitOlen <101823935+PriitOlen@users.noreply.github.com>
2024-04-26 13:57:46 +0300

    Update index.php

--- index.php
+++ index.php
@@ -4,5 +4,6 @@
   </head>
   <body>
     <h1>Meie projekti pealeht</h1>
+    <img>https://static.giga.de/wp-content/uploads/2024/04/kurwa-bober-GettyImages-92911783-rcm480x270u.jpg</img>
   </body>
 </html>
